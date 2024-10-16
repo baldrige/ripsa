ed0c7ef3ea37e687ef709b436600ff0d7291eca3
Tommy Bennett <tommy@Tommys-MacBook-Pro.local>
2024-10-15 23:07:00 -0500

    Adding oyez API functions

---
+++ R/oyez.R
@@ -0,0 +1,15 @@
+oyez_lookup <- function(term, docket) {
+  url <- "https://api.oyez.org/cases/"
+  url2 <- paste0(url, term, "/", docket)
+  req <- request(url2)
+  resp <- req_perform(req)
+  case <- resp |> resp_body_json()
+  return(case)
+}
+
+oyez_get_oa_audio <- function(term, docket) {
+  case <- oyez_lookup(term, docket)
+  href <- map(case$oral_argument_audio, "href")
+  files <- map(href, \(x) pluck(resp_body_json(req_perform(request(x))), "media_file", 1, "href"))
+  return(files)
+}
